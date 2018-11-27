41190aaf7ceca3eea3ee3615535a5616b629c398
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-11-26 21:02:30 -0800

    update

--- week7_6/week7_6/Source.cpp
+++ week7_6/week7_6/Source.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() {
 	int n;
-	cin >> n;
-	cout << n / 100 << endl;
-	cout << (n % 100) / 50 << endl;
-	cout << (n % 50) / 20 << endl;
-	cout << (n % 20) / 10 << endl;
-	cout << (n % 10) / 5 << endl;
-	cout << (n % 5) << endl;
+	while (cin >> n&&n<1000) {
+		cout << n / 100 << endl;
+		cout << (n % 100) / 50 << endl;
+		cout << (n % 50) / 20 << endl;
+		cout << (n % 20) / 10 << endl;
+		cout << (n % 10) / 5 << endl;
+		cout << (n % 5) << endl;
+	}
 	return 0;
 }
