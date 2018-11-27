2213d3b49fb1976b470c0b82e5a9c3658e7c0893
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-11-26 21:20:44 -0800

    additional test program

--- week7_6/week7_6/Source.cpp
+++ week7_6/week7_6/Source.cpp
@@ -11,5 +11,15 @@ int main() {
 		cout << (n % 100 % 50 % 20 % 10) / 5 << endl;
 		cout << n % 5 << endl;
 	}
+	
+	//test program
+	/*
+	int n;
+	for (n = 0; n < 1000; n++) {
+		if ((n % 100 % 50 % 20) / 10 != (n % 20) / 10)
+			break;
+	}
+	cout << n << endl;
+	*/
 	return 0;
 }
