8a1ceb48570446b8bbc05f435edb763b3b4e7566
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-11-26 20:51:19 -0800

    week7 question 6

---
+++ week7_6/week7_6/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << n / 100 << endl;
+	cout << (n % 100) / 50 << endl;
+	cout << (n % 50) / 20 << endl;
+	cout << (n % 20) / 10 << endl;
+	cout << (n % 10) / 5 << endl;
+	cout << (n % 5) << endl;
+	return 0;
+}
